5c9e3a08f3335fce736a01ed2324e9485c6ae218
ikarusbuszok <115149206+ikarusbuszok@users.noreply.github.com>
2023-02-01 14:41:31 +0100

    kesz

---
+++ main.py
@@ -0,0 +1,8 @@
+kartya=int(input("Húzz egy lapot"))
+while kartya <= 21:
+    if kartya < 21:
+        print("Nyert")
+    elif kartya == 21:
+        print("Vesztett")
+    else:
+        print("Vesztett")

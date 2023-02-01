3e242360898fd7de2fa9ebd7df1d924f6fd15c15
ikarusbuszok <115149206+ikarusbuszok@users.noreply.github.com>
2023-02-01 14:56:37 +0100

    javítva: kesz

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-kartya=int(input("Húzz egy lapot"))
-while kartya <= 21:
-    if kartya < 21:
-        print("Nyert")
-    elif kartya == 21:
-        print("Vesztett")
-    else:
-        print("Vesztett")
+def kartyaosztas():
+    pass
+#kartyaosztas
+
+
+
+
+
+def eredmeny():
+    pass
+#eredmeny
+
+
+
